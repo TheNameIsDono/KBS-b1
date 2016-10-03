609f5a3add6f520a843b05b598271ca8d8fb87b0
TheNameIsDono <donovanwalter@msn.com>
2016-10-03 12:01:58 +0200

    Speler met een kappa emote

--- KBS1b/KBS1b/Form1.cs
+++ KBS1b/KBS1b/Form1.cs
@@ -12,42 +12,50 @@ using System.Windows.Forms;
 
 namespace KBS1b {
     public partial class Form1 : Form {
-        private int x = 100;
-        private int y = 100;
-        private int maxX = 100; //speelveld
-        private int maxY = 100; //speelveld
-        private int 
-       
-        
+        private Image img;
+        private PointF point;
+        private int x = 20;
+        private int y = 20;
+        private int maxX = 500; //speelveld
+        private int maxY = 500; //speelveld
+
+
+
 
         public Form1() {
             InitializeComponent();
-            
+            this.img = Image.FromFile(@"E:\GitHub\KBS-b1\KBS1b\KBS1b\kappa.png"); ;
+            this.point = new PointF(x, y);
 
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-           
+
 
         }
-        private void collision() {
+        public void collision() {
             if (x <= 0) {
                 x = 0;
-            }else if(x >= maxX){
+            } else if (x >= maxX) {
                 x = maxX;
             }
             if (y <= 0) {
                 y = 0;
-            }else if (y >= maxY) {
+            } else if (y >= maxY) {
                 y = maxY;
             }
+
+
+
         }
+
         protected override void OnPaint(PaintEventArgs e) {
             collision();
             base.OnPaint(e);
             Graphics dc = e.Graphics;
             Pen p = new Pen(Color.Black, 20);
-            dc.DrawRectangle(p, 10 + x,10 + y,20,20);
+            //dc.DrawRectangle(p, 10 + x,10 + y,20,20);
+            dc.DrawImage(img, point);
         }
         //private void Form1_ResizeEnd(object sender, EventArgs e) {
         //    boundary[1] = new Point(this.Width, 0);
