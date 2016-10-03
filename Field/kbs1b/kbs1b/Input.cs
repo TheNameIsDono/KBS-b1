7d222f644b1dbd50c9055b151f7a768ffdd16c3b
Styndekoning <stijn.de.koning@windesheim.nl>
2016-10-03 15:56:08 +0200

    Field

---
+++ Field/kbs1b/kbs1b/Input.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kbs1b
+{
+    
+    class Input
+    {
+
+        
+        public int Up { get; set; }
+        public int Left { get; set; }
+        public int Down { get; set; }
+        public int Right { get; set; }
+
+        
+
+        public Input(char up, char left, char down, char right)
+        {
+
+            this.Up = Convert.ToInt32(up);
+            this.Left = Convert.ToInt32(left); 
+            this.Down = Convert.ToInt32(down);
+            this.Right = Convert.ToInt32(right);
+            Console.WriteLine(Up + "\n" + Left + "\n" + Down + "\n" + Right);
+
+        }
+    }
+
+}
+
