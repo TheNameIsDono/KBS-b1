09307b271ad7dc8ffd3c835c4e1827556f33fde6
TimVorsselman <tim.vorsselman@gmail.com>
2016-10-08 15:25:00 +0200

    Hoofdmenu (Start + Exit)
    Hoofdmenu met een werkende Start en Exit knop

--- KBS1b/KBS1b/Program.cs
+++ KBS1b/KBS1b/Program.cs
@@ -14,9 +14,26 @@ namespace kbs1b
         [STAThread]
         static void Main()
         {
+            int x = 0;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+            HoofdMenu hMenu = new HoofdMenu();
+            Application.Run(hMenu); 
+            if(hMenu.getSpelGestart())
+            {
+                Application.Run(new Form1());
+            }
+            if (hMenu.getExitGeklikt()) { x = 1; }
+            while(x < 1)
+            {
+                HoofdMenu hoMenu = new HoofdMenu();
+                Application.Run(hoMenu);
+                if (hoMenu.getSpelGestart())
+                {
+                    Application.Run(new Form1());
+                }
+                if(hoMenu.getExitGeklikt()) { x = 1; }
+            }
     }
+        }
 }
