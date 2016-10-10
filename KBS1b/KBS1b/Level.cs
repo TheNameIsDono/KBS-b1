0a9d9f5ee127f0f08dfce2b0fc06932ddd5eec5c
coenvdam <coenvdam@live.nl>
2016-10-10 11:17:15 +0200

    Toevoegen level klasse
    Level klasse is toegevoegd maar wordt nog niet gebruikt.

---
+++ KBS1b/KBS1b/Level.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kbs1b
+{
+    class Level
+    {
+        private Player player;
+        private List<Obstacle> obstacles;
+
+        internal Player Player
+        {
+            get { return player; }
+            set { player = value; }
+        }
+
+        public List<Obstacle> Obstacles
+        {
+            get { return obstacles; }
+        }
+        
+        public void addObstacle(Obstacle obstacle)
+        {
+            obstacles.Add(obstacle);
+        }
+
+        public Level(Player player)
+        {
+            this.player = player;
+            this.obstacles = new List<Obstacle>();
+        }
+
+    }
+}
